3a968c5709573340e9cd149db0efb4fcbf5b1f52
BradySimon <bradytsimon@gmail.com>
2024-07-14 23:26:14 -0400

    Make drawer animated using Offset widget

--- examples/animated_drawer.rs
+++ examples/animated_drawer.rs
@@ -1,22 +1,27 @@
 use iced::{
     advanced::Widget,
     widget::{button, container, horizontal_space, row, text, Space, Stack},
-    Border, Color, Element, Length, Point, Rectangle, Size, Theme, Vector,
+    Border, Color, Element, Length, Point, Rectangle, Size, Subscription, Theme, Vector,
 };
 use iced_anim::animation_builder;
 
 #[derive(Debug, Clone)]
 enum Message {
     ToggleDrawer,
+    WindowResized { width: u32, height: u32 },
 }
 
 struct State {
+    /// The current size of the window, which the drawer needs to be offset correctly.
+    window_size: Size,
+    /// Whether the drawer is currently open.
     is_drawer_open: bool,
 }
 
 impl Default for State {
     fn default() -> Self {
         Self {
+            window_size: Size::new(1024.0, 768.0),
             is_drawer_open: false,
         }
     }
@@ -28,17 +33,35 @@ impl State {
             Message::ToggleDrawer => {
                 self.is_drawer_open = !self.is_drawer_open;
             }
+            Message::WindowResized { width, height } => {
+                self.window_size = Size::new(width as f32, height as f32);
+            }
         }
     }
 
+    fn subscription(&self) -> Subscription<Message> {
+        iced::event::listen_with(|event, _, _| match event {
+            iced::Event::Window(iced::window::Event::Resized { width, height }) => {
+                Some(Message::WindowResized { width, height })
+            }
+            _ => None,
+        })
+    }
+
     fn view(&self) -> Element<Message> {
-        let drawer_button = button(text("Open Drawer")).on_press(Message::ToggleDrawer);
-        let offset_button = Offset::new(drawer_button).offset(Point::new(1000.0, 150.0));
-        drawer(self.is_drawer_open, offset_button)
+        let drawer_button =
+            container(button(text("Open Drawer")).on_press(Message::ToggleDrawer)).padding(8);
+        drawer(self.is_drawer_open, self.window_size, drawer_button)
     }
 }
 
-fn drawer<'a>(is_open: bool, content: impl Into<Element<'a, Message>>) -> Element<'a, Message> {
+fn drawer<'a>(
+    is_open: bool,
+    window_size: Size,
+    content: impl Into<Element<'a, Message>>,
+) -> Element<'a, Message> {
+    // The amount of padding put around the outside of the outer drawer container.
+    const PADDING: f32 = 8.0;
     // The width of the drawer when open
     const MAX_WIDTH: f32 = 350.0;
     let width = if is_open { MAX_WIDTH } else { 0.0 };
@@ -55,42 +78,40 @@ fn drawer<'a>(is_open: bool, content: impl Into<Element<'a, Message>>) -> Elemen
         .height(Length::Fill)
         .push(content)
         .push(
-            animation_builder((background, width), move |(background, width)| {
-                println!("Animating width: {}", width);
-                row![
-                    // Underlay
-                    button(container(Space::new(Length::Fill, Length::Fill)).center(Length::Fill))
-                        .on_press_maybe(is_open.then_some(Message::ToggleDrawer))
-                        .style(move |_, _| iced::widget::button::Style {
-                            background: Some(background.into()),
-                            ..Default::default()
-                        }),
-                    // Drawer content
-                    Offset::new(
-                        container(
-                            container(drawer_header())
-                                .style(move |theme: &Theme| iced::widget::container::Style {
-                                    background: Some(
-                                        theme.extended_palette().background.base.color.into(),
-                                    ),
-                                    border: Border::rounded(8),
-                                    ..Default::default()
-                                })
-                                .padding(8)
-                                .align_x(iced::alignment::Horizontal::Right)
-                                .fill_y()
-                                .center_x(Length::Fixed(MAX_WIDTH))
-                        )
-                        .padding(8)
-                        .style(move |_| iced::widget::container::Style {
-                            background: Some(background.into()),
-                            ..Default::default()
-                        })
+            // Underlay
+            animation_builder(background, move |background| {
+                button(container(Space::new(Length::Fill, Length::Fill)).center(Length::Fill))
+                    .on_press_maybe(is_open.then_some(Message::ToggleDrawer))
+                    .style(move |_, _| iced::widget::button::Style {
+                        background: Some(background.into()),
+                        ..Default::default()
+                    })
+                    .into()
+            }),
+        )
+        .push(
+            // Drawer content
+            animation_builder(width, move |width| {
+                Offset::new(
+                    container(
+                        container(drawer_header())
+                            .style(move |theme: &Theme| iced::widget::container::Style {
+                                background: Some(
+                                    theme.extended_palette().background.base.color.into(),
+                                ),
+                                border: Border::rounded(8),
+                                ..Default::default()
+                            })
+                            .padding(8)
+                            .fill_y()
+                            .center_x(Length::Fixed(MAX_WIDTH)),
                     )
-                    .offset(Point::new(width, 20.0))
-                ]
+                    .padding(PADDING),
+                )
+                .offset(Point::new(window_size.width - width - 2.0 * PADDING, 0.0))
                 .into()
             })
+            .motion(iced_anim::SpringMotion::Snappy)
             .animates_layout(true),
         );
 
@@ -121,7 +142,9 @@ fn drawer_header() -> Element<'static, Message> {
 }
 
 pub fn main() -> iced::Result {
-    iced::run("Animated Drawer", State::update, State::view)
+    iced::application("Animated Drawer", State::update, State::view)
+        .subscription(State::subscription)
+        .run()
 }
 
 struct Offset<'a, Message, Theme, Renderer>
