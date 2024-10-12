cc0e7a48c5d35d73763224918c98ea8ca6b3d6b4
BradySimon <bradytsimon@gmail.com>
2024-10-12 10:25:01 -0400

    Add docs to AnimatedState and widget module

--- iced_anim/src/widget.rs
+++ iced_anim/src/widget.rs
@@ -1,4 +1,10 @@
+//! A collection of widgets whose style are implicitly animated.
+//!
+//! This means the style of these widgets animate changes automatically without any further work
+//! from the user. These widgets are generally modified versions of built-in widgets in Iced that
+//! have been fitted to include animations by default.
 mod animated_state;
 pub mod button;
 
+pub use animated_state::AnimatedState;
 pub use button::{button, Button};
--- iced_anim/src/widget/animated_state.rs
+++ iced_anim/src/widget/animated_state.rs
@@ -1,3 +1,110 @@
+//! Helps manage animating styles for widgets.
+//!
+//! The [`AnimatedState`] internally keeps track of a widget's status, theme, and style, and then
+//! updates the animated style based on these values. This struct can be built into a widget's
+//! internal state and used to manage style animations.
+//!
+//! There are a few steps to using this struct:
+//!
+//! 1. Implement `get_initial_status` and `get_status` functions for your widget. You'll use these
+//!    to give the initial and current status to the [`AnimatedState`] so it can properly update
+//!    the animated style. For example, a button might have an initial status fn look like this:
+//!    ```no_run
+//!    # struct Button { on_press: Option<fn()> }
+//!    # enum Status { Active, Disabled }
+//!    # impl Button {
+//!
+//!    fn get_initial_status(&self) -> Status {
+//!        if self.on_press.is_some() {
+//!            Status::Active
+//!        } else {
+//!            Status::Disabled
+//!        }
+//!    }
+//!    # }
+//!    ```
+//!    For the current status, you can include any other fields that may be useful for determining
+//!    the current status. For a button, this would be something like the internal widget state
+//!    for tracking whether the button is pressed, and the cursor + layout for hover states.
+//! 2. Add a [`SpringMotion`] field to your widget so users can change how the animation behaves.
+//!    Then, add a builder function for updating it, e.g.
+//!    ```no_run
+//!    # use iced_anim::SpringMotion;
+//!    # struct Button { motion: SpringMotion }
+//!    # impl Button {
+//!    /// Sets the motion that will be used by animations.
+//!    pub fn motion(mut self, motion: SpringMotion) -> Self {
+//!        self.motion = motion;
+//!        self
+//!    }
+//!    # }
+//!    ```
+//! 3. Add an `AnimatedState<Status, Theme, Style>` field to your widget's state, including the
+//!    theme as a generic type. For example, a button state might look like this:
+//!    ```no_run
+//!    # use iced_anim::widget::AnimatedState;
+//!    # type Status = iced::widget::button::Status;
+//!    # type Style = iced::widget::button::Style;
+//!    #[derive(Debug, Clone, PartialEq)]
+//!    struct State<Theme> {
+//!        is_pressed: bool,
+//!        animated_state: AnimatedState<Status, Theme, Style>,
+//!    }
+//!    ```
+//! 4. Update any other references to `State` to use the correct generics, like `State<Theme>`.
+//!    Also make sure to include the following bounds on the `Theme` generic type:
+//!    - 'static + Catalog + Default + Clone + PartialEq
+//!    We need `'static` because `Widget::tag` requires a static type, and unfortunately for now
+//!    we must store the theme in the [`AnimatedState`] because we can't access it in `on_event`
+//!    where we would want to update the animated style. We need `Default + Clone + PartialEq` to
+//!    set a default value on first render, clone the theme when updating the animated style, and
+//!    compare the theme for updates.
+//!
+//!    > Note: in the future, I hope that we can remove the extra theme field and bounds by having
+//!    > access to the current theme in `on_event`, where we have mutable access to widget state.
+//!    > but for now, we have to track the theme in the [`AnimatedState`] and update it in `draw`.
+//! 5. Update [`iced::advanced::Widget::state`] to get the default theme and initial status, then
+//!    build your initial style from them. Then you can pass those into [`AnimatedState::new`] with
+//!    the [`AnimatedState::with_motion`] create a new instance for your widget state.
+//! 6. Update [`iced::advanced::Widget::diff`] to create a new style based on the theme and status,
+//!    then call [`AnimatedState::diff`] to update the motion and animated style if necessary.
+//!    ```ignore
+//!    fn diff(&self, tree: &mut Tree) {
+//!        // Diff the animated state with a potentially new style.
+//!        let state = tree.state.downcast_mut::<State<Theme>>();
+//!        let new_style = state
+//!            .animated_state
+//!            .theme()
+//!            .style(&self.class, state.animated_state.status().clone());
+//!        state.animated_state.diff(self.motion, new_style);
+//!        // Diff the rest of your widget state as necessary.
+//!        tree.diff_children(std::slice::from_ref(&self.content));
+//!    }
+//!    ```
+//! 7. Update [`iced::advanced::Widget::draw`] to get the current style from the animated state
+//!    instead of manually calculating the style on each draw. Use [`AnimatedState::current_style`]
+//!    and pass in the current theme and a callback to generate the style based on the theme and
+//!    status, and it'll return a reference to the latest animated style.
+//! 8. Update [`iced::advanced::Widget::on_event`] to call [`AnimatedState::needs_redraw`] to
+//!    determine if the widget needs to redraw. If a redraw is needed, then use the shell to
+//!    request a redraw on the next frame. This may vary based on your widget, but it will
+//!    generally look like this:
+//!    ```ignore
+//!    # use iced::window;
+//!    // Redraw anytime the status changes and would trigger a style change.
+//!    let state = tree.state.downcast_mut::<State<Theme>>();
+//!    let status = self.get_status(&state, cursor, layout);
+//!    let needs_redraw = state
+//!        .animated_state
+//!        .needs_redraw(status, |theme, status| theme.style(&self.class, *status));
+//!
+//!    if needs_redraw {
+//!        shell.request_redraw(window::RedrawRequest::NextFrame);
+//!    }
+//!    ```
+//! 9. Finally, ensure your widget handles [`iced::window::Event::RedrawRequested`] events by
+//!    calling [`AnimatedState::tick`] to update the animated style with the current time. This
+//!    is how the animated state can update the style over time.
 use std::{
     cell::{Ref, RefCell},
     time::Instant,
